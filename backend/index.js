1ec8e4a24b0d6cccbef7294009807942b9021adf
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-02 18:55:37 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
-// --- Rotas da Aplicação ---
 const authRoutes = require('./routes/authRoutes');
 const veiculoRoutes = require('./routes/veiculoRoutes');
 const viagemRoutes = require('./routes/viagemRoutes');
@@ -13,11 +12,9 @@ const pagamentoRoutes = require('./routes/pagamentoRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 const relatorioRoutes = require('./routes/relatorioRoutes');
 
-// --- Inicialização do Express ---
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// --- Middlewares Essenciais ---
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
